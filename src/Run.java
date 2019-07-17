dd14062f21fe9c4010a437a51895f1c5415c7045
rafael <rafael.riosgarcia@ashbury.ca>
2019-07-16 23:09:56 -0500

    created basic fxml file or tasks, created fxml file controller and changed main to be able to display fxml file properly note, add comments

--- src/Run.java
+++ src/Run.java
@@ -1,13 +1,23 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Run  extends Application {
     private static Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        stage=primaryStage;
-        stage.show();
+
+        Parent root = FXMLLoader.load(getClass().getResource("Tasker.fxml"));
+//        fxmlLoader.setResources(ResourceBundle.getBundle("bundles.MyBundle",locale));
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
---
+++ src/TaskerController.java
@@ -0,0 +1,21 @@
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TaskerController implements Initializable {
+
+    @javafx.fxml.FXML
+    private javafx.scene.control.ListView taskListView;
+    @javafx.fxml.FXML
+    private javafx.scene.control.TextField taskTextField;
+    @javafx.fxml.FXML
+    private javafx.scene.control.ColorPicker TaskColorPicker;
+    @javafx.fxml.FXML
+    private javafx.scene.control.DatePicker taskDatePicker;
+
+
+    public void initialize(URL url, ResourceBundle rb){
+
+    }
+
+}
